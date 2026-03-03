96f4bf2740925e020105bf1e117d59b725973cdb
Chawakorn Chaimongkol <107240946+R4p1Z@users.noreply.github.com>
2026-03-03 17:26:32 +0700

    Upload the 2nd day of learning

---
+++ print_len_name.py
@@ -0,0 +1,25 @@
+# Variables in Python
+first_name = 'Chawakorn'
+last_name = 'Chaimongkol'
+country = 'Thailand'
+city = 'Nonthaburi'
+age = 23
+is_married = False
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+    'first_name' : 'Chawakorn',
+    'last_name' : 'Chaimongkol',
+    'country' : 'Thailand',
+    'city' : 'Nonthaburi'
+   }
+
+print('First name: ', first_name)
+print('First name length: ', len(first_name))
+print('Last name: ', last_name)
+print('Last name length:', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Personal Information: ', person_info)

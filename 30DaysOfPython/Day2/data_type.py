7c3423b502ad7f0026de5edd94c58cde59ca517e
Chawakorn Chaimongkol <107240946+R4p1Z@users.noreply.github.com>
2026-03-03 17:32:04 +0700

    Re-upload the entire project.

---
+++ 30DaysOfPython/Day2/data_type.py
@@ -0,0 +1,20 @@
+# Different python data types
+# Let's declare variables with various data types
+
+first_name = 'Chawakorn'     # str
+last_name = 'Chaimongkol'       # str
+country = 'Thailand'         # str
+city= 'Nonthaburi'            # str
+age = 23                   # int, it is not my real age, don't worry about it
+
+# Printing out types
+print(type('Chawakorn'))          # str
+print(type(first_name))          # str
+print(type(10))                  # int
+print(type(3.14))                # float
+print(type(1 + 1j))              # complex
+print(type(True))                # bool
+print(type([1, 2, 3, 4]))        # list
+print(type({'name':'Chawakorn'})) # dict
+print(type((1,2)))               # tuple
+print(type(zip([1,2],[3,4])))    # zip
